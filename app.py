82dd6b8c72f4f12527da0086f4606d3863bdfbde
Adarsh kumar <akdubay45@gmail.com>
2023-07-14 20:54:37 +0530

    Update app.py
    Added registration page .

--- app.py
+++ app.py
@@ -1,19 +1,14 @@
 from flask import *
 import pymysql
 import os
+import re
 
 app = Flask(__name__)
-
-# Secret key for session management
 app.secret_key = os.environ['SECRET_KEY']
-
-# Configure MySQL connection
 app.config['MYSQL_HOST'] = os.environ['MYSQL_HOST']
 app.config['MYSQL_USER'] = os.environ['MYSQL_USER']
 app.config['MYSQL_PASSWORD'] = os.environ['MYSQL_PASSWORD']
 app.config['MYSQL_DB'] = os.environ['MYSQL_DB']
-
-# Establish a connection to MySQL
 mysql = pymysql.connect(
     host=app.config['MYSQL_HOST'],
     user=app.config['MYSQL_USER'],
@@ -21,21 +16,16 @@ mysql = pymysql.connect(
     db=app.config['MYSQL_DB']
 )
 
+
 @app.route('/')
 def index():
-    # Render the index.html template
     return render_template('index.html')
-
 @app.route('/home')
 def home():
-    # Render the index.html template
     return render_template('index.html')
-
 @app.route('/favicon.ico')
 def favicon():
-    # Serve the favicon.ico file from the static directory
-    return app.send_static_file('static/favicon.ico')
-
+    return app.send_static_file('static/logo.png')
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     msg = ''
@@ -53,26 +43,63 @@ def login():
             if account:
                 session['Username'] = account['Username']
                 Usernamelog = session['Username']
-                # Redirect to the dashboard route
+                msg = f'Logged in successfully as {Usernamelog}!'
+                print(msg)
                 return redirect('/dashboard')
             else:
                 msg = "Incorrect Username or Password"
         else:
             msg = "Incorrect Username or Password"
 
-    # Render the login.html template with the appropriate message
     return render_template('login.html', msg=msg)
-
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+  msg = ''
+  if request.method == 'POST' and 'Username' in request.form and 'Name' in request.form and 'Email' in request.form and 'Password' in request.form and 'Reenterpassword' in request.form and 'key' in request.form:
+        Username = request.form['Username']
+        Name = request.form['Name']
+        Email = request.form['Email']
+        Password = request.form['Password']
+        Reenterpassword = request.form['Reenterpassword']
+        key = request.form['key']
+        cursor = mysql.cursor(pymysql.cursors.DictCursor)
+        cursor.execute('SELECT * FROM user WHERE BINARY Username = %s', (Username,))
+        accountu = cursor.fetchone()
+        cursor = mysql.cursor(pymysql.cursors.DictCursor)
+        cursor.execute('SELECT * FROM user WHERE BINARY Email = %s', (Email,))
+        accounte = cursor.fetchone()
+        if Reenterpassword != Password:
+          msg = 'Password dose not match'
+        elif accountu:
+            msg = 'Username already taken !'
+        elif accounte:
+            msg = 'Account already exists !'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', Email):
+            msg = 'Invalid email address !'
+        elif not re.match(r'[A-Za-z0-9]+', Username):
+            msg = 'Username must contain only characters and numbers !'
+        elif key !=  os.environ['a_key']:
+            msg = 'Access key is wrong'
+        else:
+          cursor = mysql.cursor(pymysql.cursors.DictCursor)
+          cursor.execute('INSERT INTO user VALUES (%s, %s, %s, %s)', (Name, Username, Email, Password))
+          mysql.commit()
+          print(Name, Username, Email, Password)
+          msg = 'Account registered'
+          return render_template('login.html', msg=msg)
+          
+  elif request.method == 'POST':
+        msg = 'Please fill out the form !'
+        
+  return render_template('signup.html', msg=msg)
 @app.route('/dashboard')
 def dashboard():
     if 'Username' in session:
         Username = session['Username']
-        # Display the welcome message with the logged-in Username
         return f"Welcome, {Username}!"
     else:
-        # Redirect to the login route if the user is not logged in
-        return redirect('/'login)
+        return redirect('/login')
+
 
 if __name__ == '__main__':
-    # Run the application on host '0.0.0.0' and port 81
     app.run(host='0.0.0.0', port=81)
